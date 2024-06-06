9bddf02f900feb4e466d7edab27271572d1d0448
markchauhan <132103539+markchauhan@users.noreply.github.com>
2024-06-06 17:50:32 -0500

    Delete mac_module directory

--- mac_module/xcelium.d/run.lnx8664.18.09.d/files.ts
+++
@@ -1,4 +0,0 @@
-1717712576 /home/mcq8127/mac_module/mac.v
-1717712513 /home/mcq8127/mac_module/mac_test.v
-1717713183 /home/mcq8127/mac_module/Synthesis/mac_syn.v
-1568236065 /vol/ece303/genus_tutorial/NangateOpenCellLibrary.v
